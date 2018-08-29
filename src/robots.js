870024471e6e615a88819317e3292d6c7fdc1b38
eliseevavika <eliseeva-vika@mail.ru>
2018-08-28 17:23:13 -0700

    Finished to do this project

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+    {
+      id:1,
+        name: 'qqqqq',
+        username: 'q',
+        email: 'qqq@mmm'
+    },
+    {
+      id:2,
+        name: 'eeeee',
+        username: 'e',
+        email: 'eee@mmm'
+    },
+    {
+      id:3,
+        name: 'rrrrr',
+        username: 'e',
+        email: 'eee@mmm'
+    },
+    {
+      id:4,
+        name: 'ttttt',
+        username: 'e',
+        email: 'eee@mmm'
+    },
+    {
+      id:5,
+        name: 'yyyyyy',
+        username: 'e',
+        email: 'eee@mmm'
+    },
+    {
+      id:6,
+        name: 'uuuuu',
+        username: 'e',
+        email: 'eee@mmm'
+    },
+    {
+      id:7,
+        name: 'iiiii',
+        username: 'e',
+        email: 'eee@mmm'
+    },
+];
